17ecd3aa401fc8523027a8d4920301ac3d8ad7e9
Noel Ryan <noelgerard.ryan@gmail.com>
2018-12-17 12:46:07 +0100

    Created the hello footer and header react files

---
+++ src/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react" 
+import { defaultCoreCipherList } from "constants";
+
+const Footer = () => {
+    return (
+        <footer>
+            <h1> Made with React</h1>
+        </footer>
+    )
+}
+
+export default Footer
