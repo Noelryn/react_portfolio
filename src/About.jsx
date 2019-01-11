1028307ac8d08437d2df5e1f029b3e14656afbb2
Noel Ryan <noelgerard.ryan@gmail.com>
2019-01-11 16:56:14 +0100

    Added in the abbout page to my portfolio

---
+++ src/About.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const About = () => {
+    return (
+        <div>
+            <h1 className="content-text">About Me</h1>
+            <p className="content-text">Hi I'm Noel</p>
+        </div>
+    )
+}
+export default About 
